261aa84dde51284f03b912d8f6929f7a20211f3b
Polyaa <polinasatrajkina@gmail.com>
2024-11-15 13:23:15 +0300

    add new branch develop and main.py, classes.py

---
+++ src/classes.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+import psycopg2
+
+
+class AbsmManager(ABC):
+    @abstractmethod
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    @abstractmethod
+    def get_all_vacancies(self):
+        pass
+
+    @abstractmethod
+    def get_avg_salary(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies_with_keyword(self):
+        pass
+
+
+class DBManager:
+    def __init__(self, file_worker):
+        self.url = "https://api.hh.ru/vacancies"
+        self.headers = {"User-Agent": "HH-User-Agent"}
+        self.params = {"text": "", "page": 0, "per_page": 100}
+        self.vacancies = []
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
